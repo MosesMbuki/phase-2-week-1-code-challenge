bb24149f87b8c8ec1a136ee41ed42826f735c181
+ <moses.mutitu@student.moringaschool.com>
2025-04-13 14:42:02 +0300

    Your message

--- server/server.js
+++ server/server.js
@@ -1,15 +1,8 @@
-const jsonServer = require('json-server');
+ const jsonServer = require("json-server"); 
 const server = jsonServer.create();
-const router = jsonServer.router('db.json');
+const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
-const cors = require('cors');
-
-const PORT = process.env.PORT || 3001;
-
-server.use(cors());
+const port = process.env.PORT || 8080; 
 server.use(middlewares);
 server.use(router);
-
-server.listen(PORT, () => {
-  console.log(`JSON Server is running on port ${PORT}`);
-});
+server.listen(port);
